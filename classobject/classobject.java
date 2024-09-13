687103299ffa1b2f8ca44400dfc6d9bbfa1e29f4
sudarshan <kosudarshan15@gmail.com>
2024-09-14 00:03:46 +0530

    k

--- classobject/classobject.java
+++ classobject/classobject.java
@@ -5,6 +5,12 @@ public class classobject {
     public static void main(String[] args){
         classobject cl=new classobject();
         //access the attributes of class//
-        System.out.println(cl.x);
+
+        System.out.println("access the values:"  +cl.x);
+
+        //modify the values in the class//
+        cl.x=44;
+
+        System.out.println("modify the values using an object:" +cl.x);
     }
 }
