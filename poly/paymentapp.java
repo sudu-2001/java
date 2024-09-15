982e8cd3ebdf42eae9c870b716f3a2114c52cb21
sudarshan <kosudarshan15@gmail.com>
2024-09-16 00:15:02 +0530

    l

--- poly/paymentapp.java
+++ poly/paymentapp.java
@@ -1,22 +1,8 @@
 package poly;
 
 interface paymentmethod{
-    void paymentprocess(double ampunt);
-}
 
-class creditcard implements paymentmethod{
-
-    private String cardnumber;
-
-    public creditcard(String cardnumber){
-
-        this.cardnumber=cardnumber;
-    }
-
-    public void paymentprocess(double amount){
-
-        System.out.println("money transaction: "+ amount+"through card" + cardnumber);
-    }
+    void paymentprocess(double amount);
 
 }
 
@@ -32,38 +18,60 @@ class paypal implements paymentmethod{
 
     public void paymentprocess(double amount){
 
-        System.out.println("the amount transacted: "+amount+"through email: "+email);
+        System.out.println("The amount transacted is:" + amount + " and you have got an notification in email: "+ email);
     }
 }
 
-class paymentproccessor{
-    private paymentmethod paymentmethod1;
+class creditcard implements paymentmethod{
 
-    public paymentproccessor(paymentmethod paymentmethod1){
+    private String cardnumber;
 
-        this.paymentmethod1=paymentmethod1;
+    public creditcard(String cardnumber){
+
+        this.cardnumber=cardnumber;
     }
 
-    public void makepayment(double amount){
+    public void paymentprocess(double amount){
+
+        System.out.println("the amount is transacted: "+ amount + " through your creditcard"+ cardnumber);
+
+    }
+}
+
+class paymentprocessor{
+
+    private paymentmethod paymentmethod1;
+
+    public paymentprocessor(paymentmethod paymentmethod1){
+
+        this.paymentmethod1=paymentmethod1;
+
+    }
+
+    public void paymentprocess(double amount){
 
         paymentmethod1.paymentprocess(amount);
+
     }
 }
 public class paymentapp {
-    
+
     public static void main(String[] args) {
         
         paymentmethod paypal=new paypal("xyz@gmail.com");
 
-        paymentmethod creditcard=new creditcard("123-4567-6543");
+        paymentmethod creditcard=new creditcard("123-456-3462");
 
-        paymentproccessor process1=new paymentproccessor(creditcard);
+        paymentprocessor process1=new paymentprocessor(creditcard);
 
-        paymentproccessor process2=new paymentproccessor(paypal);
+        paymentprocessor process2=new paymentprocessor(paypal);
 
-        process1.makepayment(200);
-        
-        process2.makepayment(300);
+        process1.paymentprocess(2000);
+
+        process2.paymentprocess(3000);
+
         
     }
+    
+
 }
