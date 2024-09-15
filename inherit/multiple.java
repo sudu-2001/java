8f23b45b98870a462413bb2f18e596cacb4057b0
sudarshan <kosudarshan15@gmail.com>
2024-09-15 18:19:01 +0530

    k

---
+++ inherit/multiple.java
@@ -0,0 +1,51 @@
+package inherit;
+
+public class multiple {
+
+    String brand="ford";
+
+    public void cost(){
+
+        System.out.println("the cost ranges from 120000$ to 180000$");
+    }
+
+    public static void main(String[] args){
+
+        mustang mu=new mustang();
+
+        multiple mul=new multiple();
+
+        gt g=new gt();
+
+        System.out.println("the brand of car is:"+ mul.brand + "\n"+ "the model of car is:"+ mu.model);
+
+        mu.cost();
+
+        System.out.println("The brand of car is:" + mul.brand + "\n" +"the model of car is:" + g.model);
+
+        g.cost();
+
+    }
+}
+
+class mustang extends multiple{
+
+    String model="mustang";
+
+    public void cost(){
+
+        System.out.println("the cost of mustang is: $12000$");
+    }
+}
+
+class gt extends multiple{
+
+    String model="GT";
+
+    public void cost(){
+
+        System.out.println("the cost of GT is: $160000$");
+    }
+
+
+}
