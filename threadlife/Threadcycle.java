e9b6f346c3e1a1e790db1607a3a0f7d76376eb01
sudarshan <kosudarshan15@gmail.com>
2024-09-23 20:23:41 +0530

    k

---
+++ threadlife/Threadcycle.java
@@ -0,0 +1,43 @@
+class Mythread extends Thread
+{
+
+	public void run()
+	{
+
+		for(int i=0 ; i <= 10 ; i++)
+		{
+
+			System.out.println(i + "- Running" + Thread.currentThread().getName());
+
+			try{
+				Thread.sleep(5000);
+			}
+
+			catch(InterruptedException e){
+
+				System.out.println(e.getMessage());
+
+				}
+		}
+	}
+}
+
+public class Threadcycle{
+
+	public static void main(String[] args){
+		Mythread thread1=new Mythread();
+
+		thread1.setName("thread1");
+
+		thread1.start();
+
+		Mythread thread2=new Mythread();
+
+		thread2.setName("thread2");
+
+		thread2.start();
+
+		}
+
+
+}
